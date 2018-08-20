1f5c469501caed2be7fef3c32f3d7191ab6c9ac5
ben-tai <jamin.tairea@gmail.com>
2018-08-20 20:15:02 +1200

    sction one complete-build components

---
+++ src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { RecipesListComponent } from "../recipes-list.component";
+
+@Component({
+  selector: 'app-recipes-item',
+  templateUrl: './recipes-item.component.html',
+  styleUrls: ['./recipes-item.component.css']
+})
+export class RecipesItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
